a44410f8a938f0787c2204e75329cc2f1a9aba7c
Adri9102 <Adri9102@users.noreply.github.com>
2025-03-22 11:28:08 +0000

    Deploy preview for PR 4 🛫

--- pr-preview/pr-4/js/scripts.js
+++ pr-preview/pr-4/js/scripts.js
@@ -187,20 +187,20 @@ $(document).ready(function () {
             title: "Jakub and Kinga's Wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Aug 9, 2025 10:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Aug 10, 2025 12:00'),
 
             // Event Address
             address: 'Pałac Brunów',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: ""
         }
     });
 
@@ -214,10 +214,7 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        } else {
+
             $.post('https://.../exec', data)
                 .done(function (data) {
                     console.log(data);
@@ -232,7 +229,7 @@ $(document).ready(function () {
                     console.log(data);
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
                 });
-        }
+        
     });
 
 });
